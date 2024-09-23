ee93f2df6ab00ba64cf8f227562caae449ada048
German Maldonado <germanmaldonado962@gmail.com>
2024-09-23 18:26:43 -0300

    Controller CAtegorias,vista modal

---
+++ TiendaOnline/Controllers/CategoriaController.cs
@@ -0,0 +1,118 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiendaOnline.Data;
+using TiendaOnline.Models;
+
+namespace TiendaOnline.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private readonly TiendaOnlineContext _context;
+
+        public CategoriaController(TiendaOnlineContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Lista()
+        {
+            return View(await _context.Categorias.ToListAsync());
+        }
+
+        public IActionResult Crear()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Crear(Categoria categoria)
+        {
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Add(categoria);
+                    await _context.SaveChangesAsync();
+                    TempData["AlertMessage"] = "Categoria creada";
+                    return RedirectToAction("Lista");
+                }
+                catch
+                {
+                    ModelState.AddModelError(String.Empty, "Error");
+                }
+            }
+            return View(categoria);
+        }
+
+        public async Task<IActionResult> Editar(int? id)
+        {
+            if (id == null || _context.Categorias == null)
+            {
+                return NotFound();
+            }
+
+            var categoria = await _context.Categorias.FindAsync(id);
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+            return View(categoria);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Editar(int id, Categoria categoria)
+        {
+            if (id != categoria.Id)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(categoria);
+                    await _context.SaveChangesAsync();
+                    TempData["AlertMessage"] = "Categoria actualizada ";
+                    return RedirectToAction("Lista");
+                }
+                catch (Exception ex)
+                {
+
+                    ModelState.AddModelError(ex.Message, "al actualizar");
+                }
+            }
+            return View(categoria);
+        }
+
+        public async Task<IActionResult> Eliminar(int? id)
+        {
+            if (id == null || _context.Categorias == null)
+            {
+                return NotFound();
+            }
+
+            var categoria = await _context.Categorias
+                .FirstOrDefaultAsync(m => m.Id == id);
+
+            if (categoria == null)
+            {
+                return NotFound();
+            }
+
+            try
+            {
+                _context.Categorias.Remove(categoria);
+                await _context.SaveChangesAsync();
+                TempData["AlertMessage"] = "Categoria eliminada ";
+            }
+            catch (Exception ex)
+            {
+                ModelState.AddModelError(ex.Message, "Error");
+            }
+
+            return RedirectToAction(nameof(Lista));
+        }
+
+    }
+}
---
+++ TiendaOnline/Controllers/ControladorProducto.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TiendaOnline.Data;
+
+namespace TiendaOnline.Controllers
+{
+    public class ControladorProducto : Controller
+    {
+        private readonly TiendaOnlineContext _context;
+
+        public ControladorProducto(TiendaOnlineContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Lista()
+        {
+            return View(await _context.Productos.Include(e => e.Categoria).ToListAsync());
+        }
+
+    }
+}
